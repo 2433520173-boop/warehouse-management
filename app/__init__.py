11414f5bdceff472baa0405bcf8cb9a46dd624f7
PhuJhDong <2433520173@vaa.edu.vn>
2025-09-30 02:22:32 +0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,11 @@ from flask import Flask, flash, redirect, url_for, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, current_user
 from flask_mail import Mail
+# Sửa lại import ở đây
 from config import config
 from functools import wraps
 import os
-from datetime import datetime, timedelta # <-- Thêm timedelta
+from datetime import datetime, timedelta
 
 db = SQLAlchemy()
 mail = Mail()
@@ -13,15 +14,8 @@ login_manager = LoginManager()
 login_manager.login_view = 'main.login'
 login_manager.login_message_category = 'info'
 
-# --- BỘ LỌC CHUYỂN ĐỔI THỜI GIAN (PHẦN MỚI) ---
 def format_to_local_time(utc_dt):
-    """
-    Một bộ lọc Jinja để chuyển đổi thời gian UTC sang giờ Việt Nam (UTC+7)
-    và định dạng lại cho dễ đọc.
-    """
-    if utc_dt is None:
-        return ""
-    # Cộng thêm 7 tiếng để có giờ Việt Nam
+    if utc_dt is None: return ""
     local_dt = utc_dt + timedelta(hours=7)
     return local_dt.strftime('%H:%M ngày %d-%m-%Y')
 
@@ -34,8 +28,10 @@ def admin_required(f):
         return f(*args, **kwargs)
     return decorated_function
 
-def create_app(config_name='default'):
+# Cập nhật hàm create_app
+def create_app(config_name):
     app = Flask(__name__)
+    # Áp dụng cấu hình được chọn
     app.config.from_object(config[config_name])
 
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
@@ -44,7 +40,6 @@ def create_app(config_name='default'):
     mail.init_app(app)
     login_manager.init_app(app)
 
-    # --- Đăng ký bộ lọc thời gian với ứng dụng (DÒNG MỚI) ---
     app.jinja_env.filters['localtime'] = format_to_local_time
 
     from .routes.main_routes import main_bp
